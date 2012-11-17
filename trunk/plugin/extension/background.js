a1ebfe48010c3fd9e33948b34c23129f1b0644b3
aeharding@gmail.com <aeharding@gmail.com@a47b8807-db9e-4c18-2267-6932cf0c9d4c>
2012-11-17 16:25:49 +0000

    Added custom color ability to change the highlight color; fixed few small bugs; small amount of code cleaning

--- trunk/plugin/extension/background.js
+++ trunk/plugin/extension/background.js
@@ -3,6 +3,8 @@ chrome.extension.onRequest.addListener(
 	function(request, sender, sendResponse) {
 		if (request.localstorage == "ezNavigate") {
 			sendResponse({ezNavigate: localStorage.ezNavigate});
+		} else if(request.localstorage == "ezHighlightColor") {
+			sendResponse({ezHighlightColor: localStorage.ezHighlightColor});
 		} else if(request.tts !== undefined) {
 			chrome.tts.speak(request.tts);
 		} else if(request.ezShow == "true") {
