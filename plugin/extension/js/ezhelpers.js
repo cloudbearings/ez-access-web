5d9e13167116349d6a5155ee041800f4f818c4ce
aeharding <aeharding@gmail.com>
2013-06-07 06:11:27 +0000

    Pronouncing working (issue 35). I use the following form for a JSON dictionary:
    {
        "JSON": "testing",
        "key2": "replacement2"
    }
    
    Where the file is loaded at the loading of the page via a 'data-ez-pronounce' for lack of a better alternative ATM. Can be easily changed later.
    
    So an HTML file showing for it works could be like this (for example):
    
    <body data-ez data-ez-pronounce="hello.json">
    <p>JSON data</p>
    <p>json data</p>
    <p>Alexander V The Great</p>
    </body>
    
    Something I noticed: 'JSON' is changed with 'Testing', but 'json' is not. Is this the way you want it Bern, or do you want to change your function to accommadate this (I'd assume so)?

--- plugin/extension/js/ezhelpers.js
+++ plugin/extension/js/ezhelpers.js
@@ -9,6 +9,42 @@ function isDescendant(parent, child) {
      return false;
 }
 
+(function() {
+    var Lib = {
+        ajax: {
+            xhr: function() {
+                var instance = new XMLHttpRequest();
+                return instance;
+            },
+            getJSON: function(options, callback) {
+                var xhttp = this.xhr();
+                options.url = options.url || location.href;
+                options.data = options.data || null;
+                callback = callback ||
+                function() {};
+                options.type = options.type || 'json';
+                var url = options.url;
+                if (options.type == 'jsonp') {
+                    window.jsonCallback = callback;
+                    var $url = url.replace('callback=?', 'callback=jsonCallback');
+                    var script = document.createElement('script');
+                    script.src = $url;
+                    document.body.appendChild(script);
+                }
+                xhttp.open('GET', options.url, true);
+                xhttp.send(options.data);
+                xhttp.onreadystatechange = function() {
+                    if (xhttp.status == 200 && xhttp.readyState == 4) {
+                        callback(xhttp.responseText);
+                    }
+                };
+            }
+        }
+    };
+
+    window.Lib = Lib;
+})()
+
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
