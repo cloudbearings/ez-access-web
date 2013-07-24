f9bc10485668a945a0276249c25139f5faa56b88
aeharding <aeharding@gmail.com>
2013-07-24 04:45:47 +0000

    Code cleanup and warning when reached the top/bottom of the page.
    Some functionality may be broken in ez_enter while transitioning to revamp

--- plugin/extension/js/ezhelpers.js
+++ plugin/extension/js/ezhelpers.js
@@ -174,6 +174,19 @@ function parseOrphanedText(paragraphTags) {
 }
 
 /**
+ * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+ * @param obj1
+ * @param obj2
+ * @returns obj3 a new object based on obj1 and obj2
+ */
+function merge_options(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
+/**
  * Utility function to check if the passed object is an Array
  * From: http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
  * @param {object} param The object to be tested if it is an array.
