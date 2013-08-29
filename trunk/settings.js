20aa5d3ffff4eaf5e49173d9bc330f33bed8b7f7
aeharding@gmail.com <aeharding@gmail.com@a47b8807-db9e-4c18-2267-6932cf0c9d4c>
2013-08-29 19:01:16 +0000

    Better alert formatting.

--- trunk/settings.js
+++ trunk/settings.js
@@ -12,59 +12,59 @@ var alerts = {
     "top": [
         {"value": ""},
         {"value": "First item on screen."},
-        {"value": "First item on screen. Use the Down arrow button."},
-        {"value": "You are on the first item on this screen Use the Down arrow button to move back down through the rest of the items"},
-        {"value": "You are still on the first item at the top of this screen. Please, use the Down arrow button to move back down through the rest of the items on this screen"},
+        {"value": "First item on screen.<br>Use the Down arrow button."},
+        {"value": "You are on the first item on this screen.<br>Use the Down arrow button to move back down through the rest of the items"},
+        {"value": "You are still on the first item at the top of this screen.<br>Please, use the Down arrow button to move back down through the rest of the items on this screen"},
         {"value": "The Down arrow button is just below the button that you most recently pressed."}
     ],
     "bottom": [
         {"value": ""},
         {"value": "Last item on screen."},
-        {"value": "Last item on screen. Use the Up arrow button."},
-        {"value": "You are on the last item on this screen Use the Up arrow button to move back down through the rest of the items."},
-        {"value": "You are still on the last item at the bottom of this screen. Please, use the Up arrow button to move back down through the rest of the items on this screen."},
+        {"value": "Last item on screen.<br>Use the Up arrow button."},
+        {"value": "You are on the last item on this screen.<br>Use the Up arrow button to move back down through the rest of the items."},
+        {"value": "You are still on the last item at the bottom of this screen.<br>Please, use the Up arrow button to move back down through the rest of the items on this screen."},
         {"value": "The Up arrow button is just above the button that you most recently pressed."}
     ]
 };
 var sounds = [
-{
-"name": "action",
-"src": "earcons/action.wav"
-},
-{
-"name": "action-none",
-"src": "earcons/action-none.wav"
-},
-{
-"name": "action-check",
-"src": "earcons/action-check.wav"
-},
-{
-"name": "action-uncheck",
-"src": "earcons/action-uncheck.wav"
-},
-{
-"name": "nav-interactive",
-"src": "earcons/nav-interactive.wav"
-},
-{
-"name": "nav-move",
-"src": "earcons/nav-move.wav"
-},
-{
-"name": "nav-checked",
-"src": "earcons/nav-checked.wav"
-},
-{
-"name": "nav-unchecked",
-"src": "earcons/nav-interactive.wav"
-},
-{
-"name": "pagechange",
-"src": "earcons/pagechange.wav"
-},
-{
-"name": "nav-alert",
-"src": "earcons/alert.wav"
-}
+    {
+        "name": "action",
+        "src": "earcons/action.wav"
+    },
+    {
+        "name": "action-none",
+        "src": "earcons/action-none.wav"
+    },
+    {
+        "name": "action-check",
+        "src": "earcons/action-check.wav"
+    },
+    {
+        "name": "action-uncheck",
+        "src": "earcons/action-uncheck.wav"
+    },
+    {
+        "name": "nav-interactive",
+        "src": "earcons/nav-interactive.wav"
+    },
+    {
+        "name": "nav-move",
+        "src": "earcons/nav-move.wav"
+    },
+    {
+        "name": "nav-checked",
+        "src": "earcons/nav-checked.wav"
+    },
+    {
+        "name": "nav-unchecked",
+        "src": "earcons/nav-interactive.wav"
+    },
+    {
+        "name": "pagechange",
+        "src": "earcons/pagechange.wav"
+    },
+    {
+        "name": "nav-alert",
+        "src": "earcons/alert.wav"
+    }
 ];
